763f3ad52c7405abef374e855781faeeb2d24f5a
smartone627 <abdelwahedmodest@gmail.com>
2025-03-14 10:54:39 +0000

    add   frontend   folder   to    the   project

--- media_buying/simulation/admin.py
+++ media_buying/simulation/admin.py
@@ -1,8 +1,8 @@
 # simulation/admin.py
 from django.contrib import admin
-from .models import Campaign, Keyword, Result
+from .models import Campaign, Keyword, Result,UploadedFile
 
 admin.site.register(Campaign)
 admin.site.register(Keyword)
 admin.site.register(Result)
-
+admin.site.register(UploadedFile)
